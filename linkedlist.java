cc73544659c891e5812e99b2d98c18f6aec29033
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2021-09-09 01:17:32 +0530

    first commit

---
+++ linkedlist.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class linkedlist{
+	public static void main(String[] args) {
+		//This entire Solutions Set is provided by Sparsh Sharma, GitHub: https://github.com/sparshsharma2510
+		//If you find this useful in any way, Don't forget to star this repository
+		//Also, if you find any errors, try rasing an issue and feel free to contribute
+	}
+	/***********Q1. Program for reversing the linked list*****************/
+	public static ListNode reverseLinkedListIteratively(ListNode head){
+		ListNode prev = null;	//this will be the reference to previous node
+		ListNode curr = head;	//this is the current node pointer
+
+		while(curr != null){	//iterate till the end of the list
+			//store the next reference, because this will be broken for the current node
+			//and will be attached to the prev reference
+			ListNode next = curr.next;
+
+			curr.next = prev;	//attach the next pointer to the previous one
+			prev = curr;	//set the prev to curr node
+			curr = next;	//set the curr node to next
+			//(NOTE: Here, curr.next is not equal to next, beacuse we changed the reference and setted to prev)
+		}
+
+		return prev;
+	}
+
+	public static ListNode reverseLinkedListRecursively(ListNode head){
+		//Make a call to the recursive function
+		return revList(null, head);
+	}
+
+	public static ListNode revList(ListNode prev, ListNode curr){
+		//Once you understand the iterative version, recursive will be cakewalk
+		if(curr == null)
+			return prev;
+		//firstly store the next pointer
+		ListNode next = curr.next;
+		//Now since we have the access to the next pointer, we can now break it
+		//and attach it to the prev node
+		curr.next = prev;
+		//now recur over the remaning list and pass curr node as prev and next as curr
+		return revList(curr,next);
+	}
+}
