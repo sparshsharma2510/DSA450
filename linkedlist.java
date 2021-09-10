b8302a1e2f4472b7604cd073ee13e9c5aded347c
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2021-09-11 00:42:40 +0530

    Added Question 5

--- linkedlist.java
+++ linkedlist.java
@@ -10,10 +10,12 @@ class ListNode {
 
 public class linkedlist{
 	public static void main(String[] args) {
-		//This entire Solutions Set is provided by Sparsh Sharma, GitHub: https://github.com/sparshsharma2510
+		//DSA 450 Sheet Link: https://drive.google.com/file/d/1FMdN_OCfOI0iAeDlqswCiC2DZzD4nPsb/view
+		//This Solution Set is provided by Sparsh Sharma, GitHub: https://github.com/sparshsharma2510
 		//If you find this useful in any way, Don't forget to star this repository
-		//Also, if you find any errors, try rasing an issue and feel free to contribute
+		//Also, if you find any errors/bugs, try rasing an issue and feel free to contribute
 	}
+	
 	/***********Q1. Program for reversing the linked list*****************/
 	public static ListNode reverseLinkedListIteratively(ListNode head){
 		ListNode prev = null;	//this will be the reference to previous node
@@ -75,6 +77,69 @@ public class linkedlist{
         return false;
 
         //Time Complexity: O(n)
+        //Space Complexity: O(1)
+    }
+
+    /***********Q4. Program for deleting a loop in the linked list*****************/
+    public static void removeLoop(ListNode head){
+    	//This is also known as loop detection algo
+        ListNode slow = head, fast = head;
+        ListNode prev = slow;
+
+        while(fast.next != null && fast.next.next != null){	//iterate over the list till over fast pointer reaches the end or becomes equal to slow
+            prev = slow;	//keep a track of prev node reference of slow
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+                break;
+        }
+        if(fast != slow)
+            return;
+        if(slow == head){	
+        //this means its a circular linkedlist, so directly do prev.next = null
+        //as prev pointed to the last node of the list
+            prev.next = null;
+            return;
+        }
+
+        //Otherwise place slow pointer in the start and move both pointers by one until they meet
+        prev = fast;
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            prev = fast;
+            fast = fast.next;
+        }
+        //It is guranteed that they will always meet at the starting point of the loop if any in all circumstances
+        //To know more about this algo and why will they meet at the starting point
+        //Refer to this youtube link
+        //https://www.youtube.com/watch?v=-YiQZi3mLq0
+        prev.next = null;
+
+        //Time Complexity : O(N)
+        //Space Complexity: O(1)
+    }
+
+    /***********Q5. Program for finding the start of a loop in the linked list*****************/
+    public static ListNode findStartNode(ListNode head){
+    	ListNode slow = head, fast = head;
+
+    	while(fast.next != null && fast.next.next != null){	//iterate over the list till over fast pointer reaches the end or becomes equal to slow
+            prev = slow;	//keep a track of prev node reference of slow
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+                break;
+        }
+        //It is guranteed that they will always meet at the starting point of the loop if any in all circumstances
+        //To know more about this algo and why will they meet at the starting point
+        //Refer to this youtube link
+        //https://www.youtube.com/watch?v=-YiQZi3mLq0
+        if(fast != slow)
+            return;
+        return slow;
+
+        //Time Complexity : O(N)
         //Space Complexity: O(1)
     }
 }
